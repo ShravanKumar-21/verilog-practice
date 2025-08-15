8e74cfb2d1189b15635f8cb3a673290da4f88098
Shravan Kumar <57145455+ShravanKumar-21@users.noreply.github.com>
2025-08-15 10:00:37 +0530

    half_adder.v

--- Half_Adder/half_adder.v
+++ Half_Adder/half_adder.v
@@ -5,16 +5,9 @@ module half_adder(sum, carry, a ,b);
      input a, b;
      output reg sum, carry;
      
-    //behavioral
-    always @(a,b)
-    begin
-        sum = a ^ b;
-        carry = a & b;
-    end
-    
     //dataflow
-    //sum = a ^ b;
-    //carry = a & b;
+    sum = a ^ b;
+    carry = a & b;
     
     
 endmodule
